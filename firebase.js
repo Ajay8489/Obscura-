e2795b88b0aac5a9acea5f6ebb11a85d84e234d1
Ajay8489 <107861807+Ajay8489@users.noreply.github.com>
2026-06-21 17:24:49 +0530

    Delete firebase.js

--- firebase.js
+++
@@ -1,6 +0,0 @@
-// Replace with your Firebase config
-import {initializeApp} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import {getFirestore} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-const firebaseConfig={};
-const app=initializeApp(firebaseConfig);
-export const db=getFirestore(app);
